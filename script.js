a3c9939d0fb8f7a722d859a03ce7800f7e39c1ae
usama-nayab <83055125+usama-nayab@users.noreply.github.com>
2021-10-18 00:31:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ var color1 = document.getElementById('color1');
 var color2 = document.getElementById('color2');
 var body = document.getElementById('gradient');
 
-const setGradient = () => body.style.background = `linear-gradient(to right , ${color1.value}  , ${color2.value} )`; 
-
+const setGradient = () => {
+  body.style.background = `linear-gradient(to right , ${color1.value}  , ${color2.value} )`;
+  css.textContent = body.style.background;
+}
 color1.addEventListener('input' , setGradient);
 color2.addEventListener('input' , setGradient);
